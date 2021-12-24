9a354f6ca68fe86c57a9d65e2fb8228673a30c04
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-12-24 22:22:07 +0530

    Create InsertNum.sol

---
+++ InsertNum.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract kudos{
+    function hello(int num) public returns(int){
+        return 31+num;
+    }
+}
+
+// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
